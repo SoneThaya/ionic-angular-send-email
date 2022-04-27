1c8bf744790acf512b191cf28d339dd43b45e374
sone <sthaya@gmail.com>
2022-04-26 20:56:44 -0700

    ionic angular send email

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmailComposerOptions } from '@awesome-cordova-plugins/email-composer/ngx';
+import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  hasAccount = false;
+  currentImage = null;
+  imageData = null;
 
-  constructor() {}
+  constructor(private emailComposer: EmailComposer) {}
 
+  async checkAccount() {
+    this.hasAccount = await this.emailComposer.hasAccount();
+  }
+
+  async captureImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera, // Camera, Photos, or Prompt
+    });
+
+    this.imageData = image.base64String;
+    this.currentImage = `data:image/jpeg;base64,${image.base64String}`;
+  }
+
+  async openEmail() {
+    const email: EmailComposerOptions = {
+      to: '',
+      cc: '',
+      attachments: [`base64:image.jpg//${this.imageData}`],
+      subject: 'my cool image',
+      body: 'yo whats going on guys',
+    };
+
+    await this.emailComposer.open(email);
+  }
 }
